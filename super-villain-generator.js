33011c523e8725d396be44a4b50473a4ee2a5ead
GOVIND KUMAR <70854788+g3vind@users.noreply.github.com>
2023-01-19 17:10:42 +0530

    Add files via upload

---
+++ super-villain-generator.js
@@ -0,0 +1,6 @@
+// including the package into our file
+const supervillains = require("supervillains");
+const superVillains = require("supervillains");
+
+let myVillainName = supervillains.random();
+console.log(myVillainName);
